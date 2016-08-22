9c309bac727274db38f4a1ea3970a7351c05ae80
Christoph Klein <christoph.t.klein@me.com>
2016-08-22 12:41:06 -0400

    Moved defaults into `System`
    Resolves #2

--- pyoz/__init__.py
+++ pyoz/__init__.py
@@ -2,16 +2,11 @@
 
 import logging
 
-
-import numpy as np
-
 from pyoz.core import System, Component
 from pyoz.potentials import LennardJones, ContinuousPotential, Coulomb
 from pyoz.thermodynamic_properties import (kirkwood_buff_integrals,
                                            excess_chemical_potential)
 from pyoz import unit
-import pyoz.unit as u
-from pyoz.unit import BOLTZMANN_CONSTANT_kB as kB
 
 __all__ = ['System', 'Component', 'unit',
 
@@ -32,63 +27,3 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 logger.info('pyOZ - version {:s}'.format(__version__))
-
-
-defaults = dict()
-
-# TODO: rework internal units to be consistent with the below
-# Units
-# =====
-# distance: nm
-# time: ps
-# mass: amu
-# charge: proton charge
-# temperature: Kelvin
-# angle: radians
-# energy: kJ/mol
-
-# Physical constants
-# ==================
-T = 300 * u.kelvin
-defaults['T'] = T
-defaults['epsilon_r'] = 78.3 * u.dimensionless
-defaults['epsilon_0'] = 8.854187817e-12 * u.coulomb ** 3 / u.joule / u.meter
-
-# Coulomb interaction factor - Bjerrum length
-# V(coul) in kT is then calculated as V = b_l * z1 * z2 / r
-# with z in elementary charge units and r in A
-defaults['bjerrum_length'] = 0.0
-
-kT = T * kB
-defaults['kB'] = kB
-defaults['kT'] = kT
-defaults['beta'] = 1 / kT
-defaults['e'] = (1 * u.elementary_charge).in_units_of(u.coulomb)
-
-# Algorithm control
-# =================
-
-# number of discretization points
-n_points_exp = 12
-defaults['n_points_exp'] = 12
-n_points = 2 ** n_points_exp
-defaults['n_points'] = n_points
-
-dr = 0.05 * u.angstrom
-defaults['dr'] = dr
-
-max_r = dr.value_in_unit(u.angstrom) * n_points
-defaults['max_r'] = max_r
-dk = np.pi / max_r
-defaults['dk'] = dk
-defaults['max_k'] = dk * n_points
-
-defaults['iteration_scheme'] = 'picard'
-defaults['mix_param'] = 0.5
-defaults['tol'] = 1e-9
-defaults['max_iter'] = 1000
-defaults['max_dsqn'] = 100.0
-
-# System info
-# ===========
-defaults['closure'] = 'hnc'
--- pyoz/core.py
+++ pyoz/core.py
@@ -12,6 +12,7 @@ from pyoz.misc import rms_normed, solver
 from pyoz.potentials import TotalPotential
 import pyoz.unit as u
 from pyoz.unit import AVOGADRO_CONSTANT_NA as Na
+from pyoz.unit import BOLTZMANN_CONSTANT_kB as kB
 
 
 def prep_input(inputs):
@@ -72,10 +73,41 @@ class Component(object):
 class System(object):
     def __init__(self, name='System', **kwargs):
         self.name = name
-        # TODO for #2: warning for unused kwargs
-        settings = prep_input(kwargs)
-        for attribute, value in settings.items():
-            setattr(self, attribute, value)
+
+        # Physical Constants
+        # ==================
+        self.T = kwargs.get('T') or 300 * u.kelvin
+        self.eps_r = kwargs.get('eps_r') or 78.3 * u.dimensionless
+        self.eps_0 = (kwargs.get('eps_0') or
+                      8.854187817e-12 * u.farad / u.meter)
+        self.kT = self.T * kB
+
+        # Coulomb interaction factor - Bjerrum length
+        # V(coul) in kT is then calculated as V = b_l * z1 * z2 / r
+        # with z in elementary charge units and r in A
+        coul_factor = 4 * np.pi * self.eps_0 * self.eps_r * self.kT
+        self.bjerrum_length = ((1 * u.elementary_charge)**2 /
+                               coul_factor).value_in_unit(u.angstroms)
+
+        # Algorithm control
+        # =================
+        n_points_exp = kwargs.get('n_points_exp') or 12
+        self.n_points = 2**n_points_exp - 1
+
+        self.dr = kwargs.get('dr') or 0.05 * u.angstrom
+
+        max_r = self.dr.value_in_unit(u.angstrom) * self.n_points
+        self.dk = np.pi / max_r
+
+        self.iteration_scheme = kwargs.get('iteration_scheme') or 'picard'
+        self.mix_param = kwargs.get('mix_param') or 0.5
+        self.tol = kwargs.get('tol') or 1e-9
+        self.max_iter = kwargs.get('max_iter') or 1000
+        self.max_dsqn = kwargs.get('max_dsqn') or 100.0
+
+        # System info
+        # ===========
+        self.closure = kwargs.get('closure') or 'hnc'
 
         # TODO: units
         dr = self.dr = self.dr.value_in_unit(u.angstroms)
@@ -90,6 +122,7 @@ class System(object):
         self.h_r = None
         self.c_r = None
         self.G_r = None
+        self.S_k = None
 
     @property
     def n_components(self):
@@ -111,10 +144,7 @@ class System(object):
                                   potentials=self.potentials)
 
     def solve(self, closure='hnc', status_updates=True):
-        self.g_r = None
-        self.h_r = None
-        self.c_r = None
-        self.G_r = None
+        self.g_r = self.h_r = self.c_r = self.G_r = self.S_k = None
 
         self._apply_potentials()
         n_components = self.n_components
@@ -179,9 +209,8 @@ class System(object):
             A = E - dft.ft_convolution_factor * C_k
             B = C_k
             H_k = solver(A, B)
-
-            S = E + H_k
-            G_k = S - E - Cs_k
+            S_k = E + H_k
+            G_k = S_k - E - Cs_k
 
             # Snap back to reality.
             for i, j in np.ndindex(n_components, n_components):
@@ -197,13 +226,12 @@ class System(object):
                 break
 
             # Iterate.
-            iter_scheme = self.iteration_scheme
-            if iter_scheme == 'picard':
+            if self.iteration_scheme == 'picard':
                 mix = self.mix_param
                 G_r = (1 - mix) * G_r_previous + mix * G_r
             else:
                 raise PyozError('Iteration scheme "{}" not yet '
-                                'implemented.'.format(iter_scheme))
+                                'implemented.'.format(self.iteration_scheme))
             if status_updates:
                 time_taken = time.time() - loop_start
                 logger.info('   {:<8d}{:<8.2f}{:<8.2e}'.format(
@@ -219,6 +247,7 @@ class System(object):
             self.h_r = g_r - 1
             self.c_r = c_r
             self.G_r = G_r
+            self.S_k = S_k
             return
 
         raise PyozError('Exceeded max # of iterations: {}'.format(n_iter))
