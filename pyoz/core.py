Fix virial pressure calculation for n-component

pyoz/core.py
@@ -34,7 +34,7 @@ class System(object):
         self.r = np.linspace(dr, self.n_pts * dr - dr, self.n_pts)
         self.k = np.linspace(dk, self.n_pts * dk - dk, self.n_pts)
         self.U_r = np.zeros(shape=(0, 0, self.n_pts))
-        self.rho = None
+        self.rho_ij = None
 
         # Results get stored after `System.solve` successfully completes.
         self.g_r = self.h_r = self.c_r = self.e_r = self.H_k = None
@@ -244,20 +244,20 @@ class System(object):
         return rhos
 
     def _set_rho_ij(self, rhos):
-        self.rho = np.zeros(shape=(self.n_components, self.n_components))
-        for i, j in np.ndindex(self.rho.shape):
+        self.rho_ij = np.zeros(shape=(self.n_components, self.n_components))
+        for i, j in np.ndindex(self.rho_ij.shape):
             rho_ij = np.sqrt(rhos[i] * rhos[j])
-            self.rho[i, j] = rho_ij
-        return self.rho
+            self.rho_ij[i, j] = rho_ij
+        return self.rho_ij
 
     def __repr__(self):
         descr = list('<{}'.format(self.name))
-        if self.rho is not None:
-            descr.append('; {} component'.format(self.rho.shape[0]))
-            if self.rho.shape[0] > 1:
+        if self.rho_ij is not None:
+            descr.append('; {} component'.format(self.rho_ij.shape[0]))
+            if self.rho_ij.shape[0] > 1:
                 descr.append('s')
             descr.append('; ρ:')
-            for rho in self.rho.diagonal():
+            for rho in self.rho_ij.diagonal():
                 descr.append(' {:.6g}'.format(rho))
         descr.append('>')
         return ''.join(descr)

pyoz/properties.py
@@ -132,20 +132,26 @@ _bhatia_thornton_combinations = OrderedDict([('number-number', _Snn),
                                              ('cc', _Scc),
 ])
 
-# TODO: generalize for multi-component
+
 def pressure_virial(system):
     """Compute the pressure via the virial route
 
     P = \rho * \beta - 2/3 * pi * int_0^inf [(r*dU/dr) * g(r) * r^2]dr
 
     """
-    r, g_r, U_r, rho, kT = system.r, system.g_r, system.U_r, system.rho, system.kT
+    r, g_r, U_r, rho_ij, kT = system.r, system.g_r, system.U_r, system.rho_ij, system.kT
     dr = r[1] - r[0]
     dUdr = (np.diff(U_r) / dr)
 
     integral = integrate(y=r[1:]**3 * g_r[:, :, 1:] * dUdr, x=r[1:])
-    pressures = rho * kT - 2/3 * np.pi * rho**2 * integral
-    return np.sum(np.tril(pressures))
+
+    rhos = np.diag(rho_ij)
+    rho = np.sum(rhos)
+
+    pressure = rho * kT
+    for i, j in np.ndindex(rho_ij.shape):
+       pressure -= 2/3 * np.pi * rhos[i] * rhos[j] * integral[i, j]
+    return pressure
 
 
 def excess_chemical_potential(system):

tests/test_properties.py
@@ -39,9 +39,15 @@ def test_isothermal_compressibility(two_component_lj):
     pass
 
 
-@pytest.mark.skipif(True, reason='Not yet implemented')
-def test_pressure_virial(two_component_lj):
-    pass
+def test_pressure_virial(one_component_lj,
+                         two_component_one_inf_dilute_lj,
+                         two_component_identical_lj):
+    P_one = oz.pressure_virial(one_component_lj)
+    P_inf = oz.pressure_virial(two_component_one_inf_dilute_lj)
+    P_two = oz.pressure_virial(two_component_identical_lj)
+
+    assert np.allclose(P_one, P_inf, atol=1e-4)
+    assert np.allclose(P_one, P_two, atol=1e-4)
 
 
 @pytest.mark.skipif(True, reason='Not yet implemented')